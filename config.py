95b6509080db7308680f54046b13354408cf6546
NikitaRasskazkin <rasskazkin64@gmail.com>
2021-01-22 16:51:15 +0400

    'add default values of db connection'

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
-from os.path import dirname, isfile, join
-from dotenv import load_dotenv
 import os
-#
-# DIR_PATH = dirname(__file__)
-# ENV_PATH = join(DIR_PATH, '.env')
-#
-# if not isfile(ENV_PATH):
-#     raise FileNotFoundError('.env not found')
-#
-# load_dotenv(ENV_PATH)
+
 environ_get = os.environ.get
-
-
+DEFAULT_DB_CONNECT = "mongodb://localhost:27017/Currencies"
+DEFAULT_DB_NAME = "Currencies"
 DB_CONNECT = environ_get('DB_CONNECT')
 DB_NAME = environ_get('DB_NAME')
--- db/connection.py
+++ db/connection.py
@@ -1,18 +1,25 @@
 from pymodm.connection import connect
 from pymongo import MongoClient
 
-from config import DB_CONNECT, DB_NAME
+from config import DB_CONNECT, DB_NAME, DEFAULT_DB_NAME, DEFAULT_DB_CONNECT
 
 
 class Connection:
+    def __init__(self):
+        if DB_CONNECT and DB_NAME:
+            self.connection_string = DB_CONNECT
+            self.db_name = DB_NAME
+        else:
+            self.connection_string = DEFAULT_DB_CONNECT
+            self.db_name = DEFAULT_DB_NAME
+
     def create_connection(self, alias: str):
         is_db_exist = self._check_db()
-        connect(DB_CONNECT, alias=alias)
+        connect(self.connection_string, alias=alias)
         return is_db_exist
 
-    @staticmethod
-    def _check_db():
-        if DB_NAME in MongoClient(DB_CONNECT).list_database_names():
+    def _check_db(self):
+        if self.db_name in MongoClient(self.connection_string).list_database_names():
             return True
         else:
             return False
